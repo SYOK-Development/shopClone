07b9fe66d306565bc72eb797698f7d325fc30dd7
sinanyagsagan <s.yagsagan.sy@gmail.com>
2022-06-14 19:29:52 +0300

    initiliaze application created with spring

---
+++ src/test/java/com/syokdevelopment/shopClone/ShopCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syokdevelopment.shopClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
